8d966fafed48b211643f02d66d3d68fdce8fe59e
Sebastian Grallert <85157205+sgrallert@users.noreply.github.com>
2023-05-04 09:53:34 +0200

    Update handler.go

--- cmd/grafana-reporter/handler.go
+++ cmd/grafana-reporter/handler.go
@@ -27,8 +27,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/sgrallert/reporter/grafana"
+	"github.com/sgrallert/reporter/report"
 	"github.com/gorilla/mux"
 )
 
