3acfbb72209c03b078faac9b349116ca2e78d2a1
Sebastian Grallert <85157205+sgrallert@users.noreply.github.com>
2023-05-04 09:52:51 +0200

    Update main.go

--- cmd/grafana-reporter/main.go
+++ cmd/grafana-reporter/main.go
@@ -22,8 +22,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/sgrallert/reporter/grafana"
+	"github.com/sgrallert/reporter/report"
 	"github.com/gorilla/mux"
 )
 
